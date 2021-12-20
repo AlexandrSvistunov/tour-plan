eeac47b70580ca7020b3acfb7aae9be0a64ee0cb
Alexandr Svistunov <alsvis2019@gmail.com>
2021-12-20 09:45:14 +0500

    bread-crumbs hover

--- js/main.js
+++ js/main.js
@@ -1,68 +1,68 @@
-$(window).scroll(function(){
-  $('.subscribe').bgscroll({
-    direction: 'bottom', // направление bottom или top
-    bgpositionx: 0, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
-    debug: false, // Режим отладки
-    min:0, // минимальное положение (в %) на которое может смещаться фон
-    max:100 // максимальное положение (в %) на которое может смещаться фон
-  });
+$(window).scroll(function() {
+    $('.subscribe').bgscroll({
+        direction: 'bottom', // направление bottom или top
+        bgpositionx: 0, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
+        debug: false, // Режим отладки
+        min: 0, // минимальное положение (в %) на которое может смещаться фон
+        max: 100 // максимальное положение (в %) на которое может смещаться фон
+    });
 
 });
 
 
 const hotelSwiper = new Swiper('.hotel__slider', {
-  // Optional parameters
-  
-  loop: true,
+    // Optional parameters
 
-  // // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
+    loop: true,
 
-  // Navigation arrows
+    // // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
     navigation: {
-    nextEl: '.hotel__swiper-button--next',
-    prevEl: '.hotel__swiper-button--prev',
-  },
+        nextEl: '.hotel__swiper-button--next',
+        prevEl: '.hotel__swiper-button--prev',
+    },
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
 
-  //  And if we need scrollbar
-  //  scrollbar: {
-  //    el: '.swiper-scrollbar',
-  //  },
+    //  And if we need scrollbar
+    //  scrollbar: {
+    //    el: '.swiper-scrollbar',
+    //  },
 });
 
 
 
 const reviewsSwiper = new Swiper('.reviews-slider', {
-  // Optional parameters
-  
-  loop: true,
+    // Optional parameters
 
-  // // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
+    loop: true,
 
-  // Navigation arrows
+    // // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
     navigation: {
-    nextEl: '.reviews__swiper-button--next',
-    prevEl: '.reviews__swiper-button--prev',
-  },
+        nextEl: '.reviews__swiper-button--next',
+        prevEl: '.reviews__swiper-button--prev',
+    },
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
 
-  //  And if we need scrollbar
-  //  scrollbar: {
-  //    el: '.swiper-scrollbar',
-  //  },
+    //  And if we need scrollbar
+    //  scrollbar: {
+    //    el: '.swiper-scrollbar',
+    //  },
 });
 
