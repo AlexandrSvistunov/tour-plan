f4e2cb1626b42567b94b599f88d6bbeec40ea40c
Alexandr Svistunov <alsvis2019@gmail.com>
2021-12-29 17:54:05 +0500

    Модальное окно

--- js/main.js
+++ js/main.js
@@ -1,79 +1,119 @@
-// Подключаем parallax эффект
+$(document).ready(function() {
 
-$(window).scroll(function() {
-    $('.subscribe').bgscroll({
-        direction: 'bottom', // направление bottom или top
-        bgpositionx: 0, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
-        debug: false, // Режим отладки
-        min: 0, // минимальное положение (в %) на которое может смещаться фон
-        max: 100 // максимальное положение (в %) на которое может смещаться фон
+    // Подключаем parallax эффект
+
+    $(window).scroll(function() {
+        $(".subscribe").bgscroll({
+            direction: "bottom", // направление bottom или top
+            bgpositionx: 0, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
+            debug: false, // Режим отладки
+            min: 0, // минимальное положение (в %) на которое может смещаться фон
+            max: 100, // максимальное положение (в %) на которое может смещаться фон
+        });
     });
 
-});
+    //
+    // Подключаем 1-й слайдер
 
-// 
-// Подключаем 1-й слайдер
+    const hotelSwiper = new Swiper(".hotel__slider", {
+        // Optional parameters
 
-const hotelSwiper = new Swiper('.hotel__slider', {
-    // Optional parameters
+        loop: true,
 
-    loop: true,
+        // // If we need pagination
+        // pagination: {
+        //   el: '.swiper-pagination',
+        // },
 
-    // // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
+        // Navigation arrows
+        navigation: {
+            nextEl: ".hotel__swiper-button--next",
+            prevEl: ".hotel__swiper-button--prev",
+        },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.hotel__swiper-button--next',
-        prevEl: '.hotel__swiper-button--prev',
-    },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
 
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-    },
+        //  And if we need scrollbar
+        //  scrollbar: {
+        //    el: '.swiper-scrollbar',
+        //  },
+    });
 
-    //  And if we need scrollbar
-    //  scrollbar: {
-    //    el: '.swiper-scrollbar',
-    //  },
-});
+    // Подключаем 2-й слайдер
+
+    const reviewsSwiper = new Swiper(".reviews-slider", {
+        // Optional parameters
+
+        loop: true,
+
+        // // If we need pagination
+        // pagination: {
+        //   el: '.swiper-pagination',
+        // },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".reviews__swiper-button--next",
+            prevEl: ".reviews__swiper-button--prev",
+        },
+
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+
+        //  And if we need scrollbar
+        //  scrollbar: {
+        //    el: '.swiper-scrollbar',
+        //  },
+    });
 
 
-// Подключаем 2-й слайдер
+    // Мобильное всплывающее меню
 
-const reviewsSwiper = new Swiper('.reviews-slider', {
-    // Optional parameters
+    var menuButton = document.querySelector(".menu-burger");
+    menuButton.addEventListener("click", function() {
+        document
+            .querySelector(".header-nav")
+            .classList.toggle("header-nav--mobile");
+    });
 
-    loop: true,
 
-    // // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.reviews__swiper-button--next',
-        prevEl: '.reviews__swiper-button--prev',
-    },
+    // Модальное всплывающее окно
 
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-    },
+    var modalButtton = $("[data-toggle=modal]"); // Ищем кнопку вызова окна
+    var closeModalButtton = $(".booking-modal__close"); // Ищем крестик close
 
-    //  And if we need scrollbar
-    //  scrollbar: {
-    //    el: '.swiper-scrollbar',
-    //  },
-});
+    // Отслеживаем клик по кнопке вызова окна
+    modalButtton.on('click', openModal);
 
-// Мобильное всплывающее меню
+    // Отслеживаем клик по крестику close
+    closeModalButtton.on('click', closeModal);
 
-var menuButton = document.querySelector(".menu-burger");
-menuButton.addEventListener("click", function() {
-    document.querySelector(".header-nav").classList.toggle("header-nav--mobile");
+    // Функция открытия окна
+    function openModal() {
+        var modalOverlay = $(".modal__overlay"); //Находим оверлей
+        var bookingModal = $(".booking-modal"); // Находим модальное окно
+        modalOverlay.addClass('modal__overlay--visible'); // Навешиваем класс-модификатор
+        bookingModal.addClass('booking-modal--visible'); // Навешиваем класс-модификатор
+    }
+
+    // Функция закрытия окна
+    function closeModal() {
+        var modalOverlay = $(".modal__overlay"); //Находим оверлей
+        var bookingModal = $(".booking-modal"); // Находим модальное окно
+        modalOverlay.removeClass('modal__overlay--visible'); // Убираем класс-модификатор
+        bookingModal.removeClass('booking-modal--visible'); // Убираем класс-модификатор
+    }
+
+    // Закрываем окно клавишей ESC
+    $(document).on('keydown', function(event) {
+        if (event.keyCode == 27) {
+            closeModal();
+        }
+    });
 });
